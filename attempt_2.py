498065bcbb9b06a146d3b26ec263d7901fa276c4
Mehedi Hasan Khondoker <mehedi.hasan@upaybd.com>
2024-12-30 14:32:58 +0600

    add other data

--- attempt_2.py
+++ attempt_2.py
@@ -1,6 +1,7 @@
 import re
 import time
 import undetected_chromedriver as uc
+from numpy.lib.utils import source
 from selenium.webdriver.common.by import By
 from seleniumbase import Driver
 from selenium import webdriver
@@ -12,6 +13,20 @@ import re
 import random
 from pathlib import Path
 import pandas as pd
+import csv
+from selenium.webdriver.common.by import By
+
+
+def write_to_file(file_name, new_data):
+    with open(file_name, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        for row in new_data:
+            writer.writerow(row)
+    print("written to file")
+
+
+
 
 
 directory = Path("downloaded_files")
@@ -41,6 +56,7 @@ options.add_argument("--disable-popup-blocking")
 options.add_argument("--accept-language=en-US,en;q=0.9")
 options.add_argument("--accept-encoding=gzip, deflate, br")
 options.add_argument("--connection=keep-alive")
+options.add_argument('--headless')
 
 
 
@@ -52,7 +68,7 @@ driver = uc.Chrome(options=options)
 
 
 
-url = "https://www.wesis.org/indicators/667"
+url = "https://www.wesis.org/indicators/1"
 driver.get(url)
 driver.maximize_window()
 time.sleep(1)
@@ -66,11 +82,13 @@ time.sleep(1)
 breadcrumb_links = driver.find_element(By.CSS_SELECTOR, 'nav[aria-label="breadcrumb"]')
 
 breadcrumb_data = breadcrumb_links.text.split("\n")
-
-social_policies = breadcrumb_data[1]
-attainment = breadcrumb_data[3]
-sub_category = breadcrumb_data[4]
-indicator = breadcrumb_data[5]
+try:
+    social_policies = breadcrumb_data[1]
+    attainment = breadcrumb_data[3]
+    sub_category = breadcrumb_data[4]
+    indicator = breadcrumb_data[5] or None
+except IndexError:
+    indicator= None
 
 
 a = driver.find_elements(By.CLASS_NAME,"info-card-content")
@@ -97,27 +115,77 @@ related_indicators = ""
 if match:
     result = match.group(1).strip()
     related_indicators = str(result)
+#
+# print("# url link is >> " + url)
+# print(social_policies)
+# print(attainment)
+# print(sub_category)
+# print(indicator)
+# print(technical_name)
+# print(scale)
+# print(description)
+# print(related_indicators)
 
-print("# url link is >> " + url)
-print(social_policies)
-print(attainment)
-print(sub_category)
-print(indicator)
-print(technical_name)
-print(scale)
-print(description)
-print(related_indicators)
+tab = driver.find_element(By.ID, 'mdc-tab-2')
+tab.click()
+
+coding_rules = driver.find_element(By.XPATH, "//div[@class='info-card-content']//p")
+coding_rules = coding_rules.text if coding_rules.text else None
+
+tab = driver.find_element(By.ID, 'mdc-tab-3')
+tab.click()
+citation = driver.find_element(By.XPATH, "//*[@id='info-tabs']/div[4]/div/div").text
+
+tab = driver.find_element(By.ID, 'mdc-tab-4')
+tab.click()
+li_elements = driver.find_elements(By.XPATH, "//*[@id='info-tabs']/div[5]/div//ul//li")
+
+li_item_list = []
+for li in li_elements:
+    a_tag = li.find_element(By.TAG_NAME, "a") if len(li.find_elements(By.TAG_NAME, "a")) > 0 else None
+
+    if a_tag:
+        li_item_list.append(f"{li.text}{a_tag.text}")
+    else:
+        li_item_list.append(f"{li.text}")
+
+
+tab = driver.find_element(By.ID, 'mdc-tab-5')
+tab.click()
+source_li_elements = driver.find_elements(By.XPATH, "//*[@id='info-tabs']/div[6]/div//ul//li")
+
+source_li_item_list = []
+for li in source_li_elements:
+    a_tag = li.find_element(By.TAG_NAME, "a") if len(li.find_elements(By.TAG_NAME, "a")) > 0 else None
+
+    if a_tag:
+        source_li_item_list.append(f"{li.text}{a_tag.text}")
+    else:
+        source_li_item_list.append(f"{li.text}")
+
+
+tab = driver.find_element(By.ID, 'mdc-tab-6')
+tab.click()
+
+misc_content = driver.find_element(By.XPATH, "//div[@class='mdc-card info info--active']//div[@class='info-card-content']")
+misc = misc_content.text  if misc_content.text  else None
+
 
 
 new_data = {
-            "Social Policies Attainment": [social_policies],
-            "Attainment": [attainment],
-            "Sub Sub-category":[sub_category],
-            "Indicaticator":[indicator],
-            "Tecnicle name":[technical_name],
-            "Description":[description],
-            "Related Indicators":[related_indicators]
-        }
+    "Social Policies": [social_policies],
+    "Attainment": [attainment],
+    "Sub Sub-category": [sub_category],
+    "Indicator": [indicator],
+    "Technical name": [technical_name],
+    "Description": [description],
+    "Related Indicators": [related_indicators],
+    "Coding Rules": [coding_rules],
+    "Citation": [citation],
+    "Related Publications": '\n'.join(li_item_list),
+    "Sources": '\n'.join(source_li_item_list),
+    "Misc": misc
+}
 
 file_path = "data.csv"
 df = pd.DataFrame(new_data)
@@ -128,11 +196,3 @@ data = pd.read_csv(file_path)
 time.sleep(3)
 
 driver.close()
-'''
-
-url = "https://www.wesis.org/indicators/663"
-id = driver.find_element(by,"session_email").send
-pas = driver.find_element(by,"session_password")
-time.sleep(200)
-driver.quit()
-'''
